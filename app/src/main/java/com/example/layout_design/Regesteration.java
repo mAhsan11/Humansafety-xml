c4ad7bca8e5d67ff52596b0ab0e10aa84be89a74
Ashir <ahsanaliahsan913839@gmail.com>
2021-06-26 18:11:57 +0500

    comit one

---
+++ app/src/main/java/com/example/layout_design/Regesteration.java
@@ -0,0 +1,14 @@
+package com.example.layout_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Regesteration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regesteration);
+    }
+}
