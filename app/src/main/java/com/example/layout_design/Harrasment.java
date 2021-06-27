d3fa4590c2f303b1eea78d253f694aab85e92258
Ashir <ahsanaliahsan913839@gmail.com>
2021-06-27 12:08:45 +0500

    comit three

---
+++ app/src/main/java/com/example/layout_design/Harrasment.java
@@ -0,0 +1,17 @@
+package com.example.layout_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class Harrasment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harrasment);
+
+    }
+}
