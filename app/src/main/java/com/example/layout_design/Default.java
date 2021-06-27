ac34d24246a3d458212687f6f0b60affc98f8053
Ashir <ahsanaliahsan913839@gmail.com>
2021-06-27 19:57:09 +0500

    comit three

--- app/src/main/java/com/example/layout_design/Default.java
+++ app/src/main/java/com/example/layout_design/Default.java
@@ -1,11 +1,13 @@
 package com.example.layout_design;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class Default extends AppCompatActivity {
@@ -21,6 +23,32 @@ public class Default extends AppCompatActivity {
         MenuInflater menuInflater= getMenuInflater();
         menuInflater.inflate(R.menu.logout_menu,menu);
         return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.logout:
+                logout();
+                return true;
+
+            case R.id.newAccount:
+                Toast.makeText(this, "Clicked on New Account!!!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.update:
+                Toast.makeText(this, "Clicked on Update Profile!!", Toast.LENGTH_SHORT).show();
+                return  true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void logout(){
+        Toast.makeText(this, "Successfully Logout!", Toast.LENGTH_SHORT).show();
 
     }
 }
