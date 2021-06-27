23ff9293356b99e004482c6c4d1cd675115e13c5
Ashir <ahsanaliahsan913839@gmail.com>
2021-06-28 00:14:37 +0500

    comit three

---
+++ app/src/main/java/com/example/layout_design/MissingChildView.java
@@ -0,0 +1,13 @@
+package com.example.layout_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MissingChildView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_missing_child_view);
+    }
+}
