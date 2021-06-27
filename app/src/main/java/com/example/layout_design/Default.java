6af2014b2b3167f84d688a4862cccd2347431cbc
Ashir <ahsanaliahsan913839@gmail.com>
2021-06-27 18:54:31 +0500

    comit three

--- app/src/main/java/com/example/layout_design/Default.java
+++ app/src/main/java/com/example/layout_design/Default.java
@@ -3,6 +3,10 @@ package com.example.layout_design;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
 
 public class Default extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class Default extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater= getMenuInflater();
+        menuInflater.inflate(R.menu.logout_menu,menu);
+        return true;
+
     }
 }
